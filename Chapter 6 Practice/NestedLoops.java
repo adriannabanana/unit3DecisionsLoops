d07a18a672baf827cbac1b1662817eea09adc363
adriannabanana <afu@NN123-HA-14.HS.sd203.org>
2015-10-29 15:09:21 -0500

    loooops

--- Chapter 6 Practice/NestedLoops.java
+++ Chapter 6 Practice/NestedLoops.java
@@ -1,8 +1,45 @@
 public class NestedLoops
 { 
     public static void loops() 
-    {  final int limit = 8 ; 
-        for ( int i = 1 ;i <=  limit ; i++)
+    {  
+        final int limit1 = 2 ; 
+        final int limit2 = 4; 
+        for ( int i = 1 ;i <=  limit1 ;i++ )
         {
-            if 
+           for (int i2 =1; i2 <= limit2 ; i2++) 
+           {
+            System.out.println(i+" "+i2);
+            }
         }
+    }
+    
+    public static void whileLoops()
+    {
+        final int limit1 = 2;
+        final int limit2 = 4;
+        int i = 1;
+        int i2 = 1;
+        
+        while (i <= limit1)
+        {
+            while (i2 <= limit2)
+            {
+                System.out.println(i + " " + i2);
+                i2++;
+            }
+            i++;
+            i2 = 1;
+        }
+    }
+    
+    public static void doLoops()
+    { 
+        final int limit1 = 2 ; 
+        final int limit2 =4 ; 
+        int i =1 ; 
+        int i2 = 1; 
+        do 
+        { 
+            
+    
+}
